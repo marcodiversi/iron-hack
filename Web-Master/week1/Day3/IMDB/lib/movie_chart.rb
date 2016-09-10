9a4e599958e86040e91982910b27a5a931f44e36
Marco Diversi <marcodiversi@gmail.com>
2016-09-10 07:39:35 +0000

    10 sept

--- Web-Master/week1/Day3/IMDB/lib/movie_chart.rb
+++ Web-Master/week1/Day3/IMDB/lib/movie_chart.rb
@@ -4,38 +4,5 @@ class MoviesChart
 		@movies = movies
 	end
 	
-	def print_chart
-		chart_container = ""
 
-		10.downto(1) do | i |
-			@movies.each do | movie |
-				if movie.rating != nil && movie.rating >= i
-					chart_container += "|#"
-				else
-					chart_container += "| "
-				end
-			end
-			chart_container += "|\n"
-		end
-
-		puts chart_container
-	end
-
-	def print_number
-		#two ways to do it
-		#1
-		# @movies.each_index { | x | print "|", x+1}
-		#2
-		(1..@movies.length).each do |num|
-			print "|#{num}"
-		end
-		print "|\n"
-	end
-
-	def list_movies
-		@movies.each_with_index do | movie, index |
-			puts "#{index+1}.#{movie.title}"
-		end
-	end
-#working not
 end
